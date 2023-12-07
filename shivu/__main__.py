a3eb1c28fcc42bdd297e438c7cc820e52588b3cd
Takievil <138558442+Takievilkun@users.noreply.github.com>
2023-12-07 22:16:29 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -116,8 +116,8 @@ async def send_image(update: Update, context: CallbackContext) -> None:
     await context.bot.send_photo(
         chat_id=chat_id,
         photo=character['img_url'],
-        caption="""A New Character Has Just Appeared Use /guess [name]
-And Add This Character In Your Collection""",
+        caption="""Gʀᴇᴀᴛ! 🎐ᴀ ɴᴇᴡ ᴡᴀɪғᴜ ʜᴀs ᴊᴜsᴛ ᴀᴘᴘᴇᴀʀᴇᴅ ᴜsᴇ /guess ɴᴀᴍᴇ 
+ᴀɴᴅ ᴀᴅᴅ ᴛʜɪs ᴡᴀɪғᴜ ɪɴ ʏᴏᴜʀ ᴄᴏʟʟᴇᴄᴛɪᴏɴ"",
         parse_mode='Markdown')
     
 async def guess(update: Update, context: CallbackContext) -> None:
