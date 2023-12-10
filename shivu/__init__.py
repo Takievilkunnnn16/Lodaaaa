331e7ad7ca5504a20a53e6f11dca30cb1912344a
Takievil <138558442+Takievilkun@users.noreply.github.com>
2023-12-10 13:43:02 +0530

    Update __init__.py

--- shivu/__init__.py
+++ shivu/__init__.py
@@ -1,9 +1,9 @@
+import os
+import telegram.ext as tg
+from pyrogram import Client
 import logging  
-from pyrogram import Client 
-
 from telegram.ext import Application
 from motor.motor_asyncio import AsyncIOMotorClient
-
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
@@ -15,27 +15,33 @@ logging.getLogger('httpx').setLevel(logging.WARNING)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(name)
 
-OWNER_ID = 1643054031
-sudo_users = ["6384296585", "1643054031"]
-GROUP_ID = -1002134049876
-TOKEN = "6883098627:AAG9_BgUgaaRhXO0oUmYabqj_pJNcsxoGpQ"
-mongo_url = "mongodb+srv://Takievil:takievil098@takievil.uallkel.mongodb.net/?retryWrites=true&w=majority"
-PHOTO_URL = ["https://telegra.ph/file/6ed59e57f91d40ef48444.jpg", "https://telegra.ph/file/a216bfefa4b2c1bd9b600.jpg"]
-SUPPORT_CHAT = "Catch_Your_waifu"
-UPDATE_CHAT = "CATCH_YOUR_WAIFU_UPDATES"
-BOT_USERNAME = "Catch_Your_Waifu_Bot"
-CHARA_CHANNEL_ID = -1002124773371
-api_id = 29593257
-api_hash = "e9a3897c961f8dce2a0a88ab8d3dd843"
+OWNER_ID = os.getenv("OWNER_ID")
+sudo_users = os.getenv("SUDO_USERS").split()
+GROUP_ID = os.getenv("GROUP_ID")
+TOKEN = os.getenv("TOKEN")
+mongo_url = os.getenv("MONGO_URL")
+PHOTO_URL = os.getenv("PHOTO_URL").split()
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
+CHARA_CHANNEL_ID = os.getenv("CHARA_CHANNEL_ID")
+API_HASH = os.getenv("API_HASH")
+API_ID = os.getenv("API_ID")
+UPDATE_CHAT = os.getenv("UPDATE_CHAT")
+BOT_USERNAME = os.getenv("BOT_USERNAME")
 
 
 application = Application.builder().token(TOKEN).build()
-shivuu = Client("Shivu", api_id, api_hash, bot_token=TOKEN)
+shivuu = Client(
+    "lmao",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=TOKEN,
+    
+    
+)
 client = AsyncIOMotorClient(mongo_url)
 db = client['Character_catcher']
-collection = db['anime_characters']
-user_totals_collection = db['user_totals']
-user_collection = db["user_collection"]
-group_user_totals_collection = db['group_user_total']
-top_global_groups_collection = db['top_global_groups']
-pm_users = db['total_pm_users']
+collection = db['anime_characters_lol']
+user_totals_collection = db['user_totals_lmaoooo']
+user_collection = db["user_collection_lmaoooo"]
+group_user_totals_collection = db['group_user_totalsssssss']
+top_global_groups_collection = db['top_global_groups'
