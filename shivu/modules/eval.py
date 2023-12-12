43b771777269a57899129eab7485a1b3be6350e4
Takievil <138558442+Takievilkun@users.noreply.github.com>
2023-12-13 00:33:00 +0530

    Update eval.py

--- shivu/modules/eval.py
+++ shivu/modules/eval.py
@@ -71,7 +71,7 @@ async def execute(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 def cleanup_code(code):
-    if code.startswith("Taki") and code.endswith("kun"):
+    if code.startswith("'''") and code.endswith("'''"):
         return "\n".join(code.split("\n")[1:-1])
     return code.strip("` \n")
 
@@ -90,7 +90,7 @@ async def do(func, bot, update):
 
     stdout = io.StringIO()
 
-    to_compile = f'async def func():\n{textwrap.indent(body, "1643054031")}'
+    to_compile = f'async def func():\n{textwrap.indent(body, " ")}'
 
     try:
         exec(to_compile, env)
