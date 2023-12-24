aa4df404e268cc9cf71c730b34b5a2ac3a794d17
Haruhiko <127282376+xclashers7720@users.noreply.github.com>
2023-12-24 16:16:59 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -1,34 +1,23 @@
 import re
 import time
 from html import escape
-from cachetools import TTLCache
 
 from telegram import Update, InlineQueryResultPhoto
-from telegram.ext import InlineQueryHandler, CallbackContext, CommandHandler 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import InlineQueryHandler, CallbackContext
 
-from shivu import user_collection, collection, application 
-
-
-all_characters_cache = TTLCache(maxsize=10000, ttl=36000)
-user_collection_cache = TTLCache(maxsize=10000, ttl=60)
+from shivu import user_collection, collection, application
 
 async def inlinequery(update: Update, context: CallbackContext) -> None:
     query = update.inline_query.query
     offset = int(update.inline_query.offset) if update.inline_query.offset else 0
 
+    
     if query.startswith('collection.'):
         user_id, *search_terms = query.split(' ')[0].split('.')[1], ' '.join(query.split(' ')[1:])
         if user_id.isdigit():
-            
-            
-            if user_id in user_collection_cache:
-                user = user_collection_cache[user_id]
-            else:
-                user = await user_collection.find_one({'id': int(user_id)})
-                user_collection_cache[user_id] = user
-
+            user = await user_collection.find_one({'id': int(user_id)})
             if user:
+                
                 all_characters = list({v['id']:v for v in user['characters']}.values())
                 if search_terms:
                     regex = re.compile(' '.join(search_terms), re.IGNORECASE)
@@ -37,19 +26,12 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
                 all_characters = []
         else:
             all_characters = []
-
-        
-    
     else:
         if query:
             regex = re.compile(query, re.IGNORECASE)
             all_characters = list(await collection.find({"$or": [{"name": regex}, {"anime": regex}]}).to_list(length=None))
         else:
-            if 'all_characters' in all_characters_cache:
-                all_characters = all_characters_cache['all_characters']
-            else:
-                all_characters = list(await collection.find({}).to_list(length=None))
-                all_characters_cache['all_characters'] = all_characters
+            all_characters = list(await collection.find({}).to_list(length=None))
 
     characters = all_characters[offset:offset+50]
     if len(characters) > 50:
@@ -68,7 +50,7 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
             user_anime_characters = sum(c['anime'] == character['anime'] for c in user['characters'])
             caption = f"<b> Look At <a href='tg://user?id={user['id']}'>{(escape(user.get('first_name', user['id'])))}</a>'s Character</b>\n\n🌸: <b>{character['name']} (x{user_character_count})</b>\n🍁: <b>{character['anime']} ({user_anime_characters}/{anime_characters})</b>\n<b>{character['rarity']}</b>\n\n<b>🆔️:</b> {character['id']}"
         else:
-            caption = f"<b>Look At This Character !!</b>\n\n🌸:<b> {character['name']}</b>\n🍁: <b>{character['anime']}</b>\n<b>{character['rarity']}</b>\n🆔️: <b>{character['id']}</b>\n\n<b>Globally Guessed {global_count} Times...</b>"
+            caption = f"<b>Look At This Character !!</b>\n\n🌸:<b> {character['name']}</b>\n🍁:<b>{character['anime']}</b>\n<b>{character['rarity']}</b>\n🆔️: <b>{character['id']}</b>\n\n<b>Globally Guessed {global_count} Times...</b>"
         results.append(
             InlineQueryResultPhoto(
                 thumbnail_url=character['img_url'],
