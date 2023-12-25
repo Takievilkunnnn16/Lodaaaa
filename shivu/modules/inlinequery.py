9a06e37acc5fa5b38f7b2eeaff8d5d9c3166a45c
takiunkwondev <152381612+takiunkwondev@users.noreply.github.com>
2023-12-26 02:31:40 +0530

    Update inlinequery.py

--- shivu/modules/inlinequery.py
+++ shivu/modules/inlinequery.py
@@ -1,23 +1,38 @@
 import re
 import time
 from html import escape
+from cachetools import TTLCache
+from pymongo import MongoClient, ASCENDING
 
 from telegram import Update, InlineQueryResultPhoto
-from telegram.ext import InlineQueryHandler, CallbackContext
+from telegram.ext import InlineQueryHandler, CallbackContext, CommandHandler 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-from shivu import user_collection, collection, application
+from shivu import user_collection, collection, application, db
+
+
+# Create indexes
+db.characters.create_index([('id', ASCENDING)])
+db.characters.create_index([('anime', ASCENDING)])
+db.characters.create_index([('img_url', ASCENDING)])
+
+all_characters_cache = TTLCache(maxsize=10000, ttl=36000)
+user_collection_cache = TTLCache(maxsize=10000, ttl=60)
 
 async def inlinequery(update: Update, context: CallbackContext) -> None:
     query = update.inline_query.query
     offset = int(update.inline_query.offset) if update.inline_query.offset else 0
 
-    
     if query.startswith('collection.'):
         user_id, *search_terms = query.split(' ')[0].split('.')[1], ' '.join(query.split(' ')[1:])
         if user_id.isdigit():
-            user = await user_collection.find_one({'id': int(user_id)})
+            if user_id in user_collection_cache:
+                user = user_collection_cache[user_id]
+            else:
+                user = await user_collection.find_one({'id': int(user_id)})
+                user_collection_cache[user_id] = user
+
             if user:
-                
                 all_characters = list({v['id']:v for v in user['characters']}.values())
                 if search_terms:
                     regex = re.compile(' '.join(search_terms), re.IGNORECASE)
@@ -31,7 +46,11 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
             regex = re.compile(query, re.IGNORECASE)
             all_characters = list(await collection.find({"$or": [{"name": regex}, {"anime": regex}]}).to_list(length=None))
         else:
-            all_characters = list(await collection.find({}).to_list(length=None))
+            if 'all_characters' in all_characters_cache:
+                all_characters = all_characters_cache['all_characters']
+            else:
+                all_characters = list(await collection.find({}).to_list(length=None))
+                all_characters_cache['all_characters'] = all_characters
 
     characters = all_characters[offset:offset+50]
     if len(characters) > 50:
@@ -48,9 +67,9 @@ async def inlinequery(update: Update, context: CallbackContext) -> None:
         if query.startswith('collection.'):
             user_character_count = sum(c['id'] == character['id'] for c in user['characters'])
             user_anime_characters = sum(c['anime'] == character['anime'] for c in user['characters'])
-            caption = f"<b> Look At <a href='tg://user?id={user['id']}'>{(escape(user.get('first_name', user['id'])))}</a>'s Character</b>\n\n🌸: <b>{character['name']} (x{user_character_count})</b>\n🍁: <b>{character['anime']} ({user_anime_characters}/{anime_characters})</b>\n<b>{character['rarity']}</b>\n\n<b>🆔️:</b> {character['id']}"
+            caption = f"<b> Look At <a href='tg://user?id={user['id']}'>{(escape(user.get('first_name', user['id'])))}</a>'s Character</b>\n\n🌸: <b>{character['name']} (x{user_character_count})</b>\n🏖️: <b>{character['anime']} ({user_anime_characters}/{anime_characters})</b>\n<b>{character['rarity']}</b>\n\n<b>🆔️:</b> {character['id']}"
         else:
-            caption = f"<b>Look At This Character !!</b>\n\n🌸:<b> {character['name']}</b>\n🍁:<b>{character['anime']}</b>\n<b>{character['rarity']}</b>\n🆔️: <b>{character['id']}</b>\n\n<b>Globally Guessed {global_count} Times...</b>"
+            caption = f"<b>Look At This Character !!</b>\n\n🌸:<b> {character['name']}</b>\n🍁: <b>{character['anime']}</b>\n<b>{character['rarity']}</b>\n🆔️: <b>{character['id']}</b>\n\n<b>Globally Guessed {global_count} Times...</b>"
         results.append(
             InlineQueryResultPhoto(
                 thumbnail_url=character['img_url'],
