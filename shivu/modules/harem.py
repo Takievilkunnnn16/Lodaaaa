b3773f0162c7ec80803196affcf0a12ab5f1c507
Takievil <138558442+Takievilkun@users.noreply.github.com>
2024-01-12 15:54:43 +0530

    Update harem.py

--- shivu/modules/harem.py
+++ shivu/modules/harem.py
@@ -15,9 +15,9 @@ async def harem(update: Update, context: CallbackContext, page=0) -> None:
     user = await user_collection.find_one({'id': user_id})
     if not user:
         if update.message:
-            await update.message.reply_text('You Have Not Guessed any Characters Yet..')
+            await update.message.reply_text('ʏᴏᴜ ʜᴀᴠᴇ ɴᴏᴛ ᴄᴀᴛᴄʜ ᴀɴʏ ʜᴜꜱʙᴀɴᴅᴏ ʏᴇᴛ..')
         else:
-            await update.callback_query.edit_message_text('You Have Not Guessed any Characters Yet..')
+            await update.callback_query.edit_message_text('ʏᴏᴜ ʜᴀᴠᴇ ɴᴏᴛ ᴄᴀᴛᴄʜ ᴀɴʏ ʜᴜꜱʙᴀɴᴅᴏ ʏᴇᴛ..')
         return
 
     characters = sorted(user['characters'], key=lambda x: (x['anime'], x['id']))
@@ -132,7 +132,7 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
 
 
 
-application.add_handler(CommandHandler(["harem", "collection"], harem,block=False))
+application.add_handler(CommandHandler(["hharem", "collection"], harem,block=False))
 harem_handler = CallbackQueryHandler(harem_callback, pattern='^harem', block=False)
 application.add_handler(harem_handler)
     
