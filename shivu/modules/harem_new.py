74e54d26ab6a0876e13735db146c8d0fa6e40ab9
takiunkwondev <152381612+takiunkwondev@users.noreply.github.com>
2024-01-22 23:17:30 +0530

    Update harem_new.py

--- shivu/modules/harem_new.py
+++ shivu/modules/harem_new.py
@@ -105,6 +105,6 @@ async def harem_callback(update: Update, context: CallbackContext) -> None:
 
     await harem(update, context, page)
 
-application.add_handler(CommandHandler("harem", harem, block=False))
+application.add_handler(CommandHandler("hharem", harem, block=False))
 harem_handler = CallbackQueryHandler(harem_callback, pattern='^harem', block=False)
 application.add_handler(harem_handler)
