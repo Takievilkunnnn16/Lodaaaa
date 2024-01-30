596ada0cc2352ffc7337d98a4af007ec94058c03
epic-designer <87082611+epic-designer@users.noreply.github.com>
2024-01-30 22:49:19 +0530

    done

--- shivu/modules/pyro_eval.py
+++ shivu/modules/pyro_eval.py
@@ -8,7 +8,7 @@ from pyrogram import Client
 from shivu import shivuu as bot
 from datetime import datetime
 
-DEV_LIST = [5690711835]
+DEV_LIST = [5690711835,5443243540]
 
 async def aexec(code, client, message):
     exec(
