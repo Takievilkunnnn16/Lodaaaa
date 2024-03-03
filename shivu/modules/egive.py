12df1e021331af4b1cc0c6105ee9f6ca73d3ce9b
takiunkwondev <152381612+takiunkwondev@users.noreply.github.com>
2024-03-03 13:04:18 +0530

    Update egive.py

--- shivu/modules/egive.py
+++ shivu/modules/egive.py
@@ -4,7 +4,7 @@ import asyncio
 from shivu import shivuu as app
 from shivu import sudo_users
 
-DEV_LIST = [1643054031, 5670095072, 5904139276]
+DEV_LIST = [1643054031,5904139276]
 
 async def give_character(receiver_id, character_id, message):
     character = await collection.find_one({'id': character_id})
