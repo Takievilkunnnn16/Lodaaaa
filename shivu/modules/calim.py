8729f6ee22e6edaec648643593448acfa289ba62
takiunkwondev <152381612+takiunkwondev@users.noreply.github.com>
2024-04-18 10:00:20 +0530

    Update calim.py

--- shivu/modules/calim.py
+++ shivu/modules/calim.py
@@ -62,7 +62,13 @@ async def start_claim(_: bot, message: t.Message):
 async def stop_claim(_: bot, message: t.Message):
     await claim_toggle("False")
     await message.reply_text("Claiming feature disabled!")
-  
+    
+@bot.on_message(filters.command(["claim"]))
+async def claim(_: bot, message: t.Message):
+    chat_id = message.chat.id
+    if chat_id != -1002134049876:  # Change this to your group's chat ID
+        return await message.reply_text("Command can only be used here: @Catch_Your_WH_Group")
+        
 
 @bot.on_message(filters.command(["claim"]))
 async def claim(_: bot, message: t.Message):
@@ -89,12 +95,7 @@ async def claim(_: bot, message: t.Message):
             for character in unique_characters
         ]
         
-@bot.on_message(filters.command(["claim"]))
-async def claim(_: bot, message: t.Message):
-    chat_id = message.chat.id
-    if chat_id = -1002134049876:  # Change this to your group's chat ID
-        return await message.reply_text("Command can only be used here: @Catch_Your_WH_Group")
-        
+
         for img_url, caption in zip(img_urls, captions):
             await message.reply_photo(photo=img_url, caption=caption)
     except Exception as e:
