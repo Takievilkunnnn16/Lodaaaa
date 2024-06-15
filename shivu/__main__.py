fd02a0ef9620e63a1d77aa3a22fe98eeb0dd2d36
takiunkwondev <152381612+takiunkwondev@users.noreply.github.com>
2024-06-15 12:16:51 +0530

    Update __main__.py

--- shivu/__main__.py
+++ shivu/__main__.py
@@ -270,8 +270,9 @@ async def guess(update: Update, context: CallbackContext) -> None:
 
     guess = ' '.join(context.args).lower() if context.args else ''
     
-    if "()" in guess or "&" in guess.lower():
-        await update.message.reply_text("You can't use '&' in your guess.")
+    if "()","🧹","👘","❄️","🏖️","🎄","🐰","🎃","✨","⚡","☂️","🏀","☔","🏇","💗","💖","💝","👩‍🚀","🕶","🥂","🎒","🌤","👨‍🚀","🎊","🌹","🏝","🎩","👙","🏸","🎮","👑","🌙","💫","👶","💍","👰‍♀","🌸","🌴","🐠","🦋","🏋‍♂️","🍽","🍰","🎸","🥊","🩺"  
+in guess.lower() or "&", "X"  in guess.lower():
+        await update.message.reply_text("You can't use all types emojis in your guess.")
         return
         
     
